2ac58af65ce53561a6648ead4385044a1b4b970c
Bishwa7 <bishwa244@gmail.com>
2026-02-09 00:50:22 +0530

    Mouse glow added in Hero section

--- ModernResponsive1/src/components/Hero.jsx
+++ ModernResponsive1/src/components/Hero.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react"
 
 export default function Hero () {
 
+    const [mousePosition, setMousePosition] = useState({x:0, y:0});
+
+    const handleMouseMove = (e) => {
+        setMousePosition({
+            x: e.x,
+            y: e.y,
+        });
+
+        // console.log(e.x, e.y)
+    };
+    
+
+    useEffect(() => {
+        // const handleMouseMove = (e) => {
+        //     console.log(e.clientX, e.clientY)
+        // }
+
+        window.addEventListener("mousemove", handleMouseMove)
+
+        return () => {
+            window.removeEventListener("mousemove", handleMouseMove)
+        }
+    },[])
+
     return (
-        <div></div>
+        <section className="relative min-h-screen flex items-center justify-center pt-16 sm:pt-20 px-4 sm:px-6 lg:px-8 overflow-hidden">
+            <div className="absolute inset-0 opacity-30" style={{
+                background: `radial-gradient(500px circle at ${mousePosition.x}px ${mousePosition.y}px, rgb(59, 130, 246, 0.30), transparent 40%)`
+            }} />
+        </section>
     )
 }
